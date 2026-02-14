1f04aefa4a9f79b320fffe7c200f5740d934757e
kaapo <kaapo.pikkarainen@gmail.com>
2026-02-14 23:24:07 +0200

    Allows ps3 clients to connect. DontNet8.0

--- Components/Blaze/AuthenticationComponent.cs
+++ Components/Blaze/AuthenticationComponent.cs
@@ -1,11 +1,11 @@
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+using Blaze2SDK;
 using Blaze2SDK.Blaze;
 using Blaze2SDK.Blaze.Authentication;
 using Blaze2SDK.Components;
 using BlazeCommon;
-using XI5;
+using SceNetNp;
 
 namespace Zamboni.Components.Blaze;
 
@@ -13,30 +13,33 @@ public class AuthenticationComponent : AuthenticationComponentBase.Server
 {
     public override Task<ConsoleLoginResponse> Ps3LoginAsync(PS3LoginRequest request, BlazeRpcContext context)
     {
-        var ticket = new XI5Ticket(request.mPS3Ticket);
+        if (!NpTicket.TryParse(request.mPS3Ticket, out NpTicket? ticket))
+        {
+            throw new BlazeRpcException(Blaze2RpcError.AUTH_ERR_INVALID_PS3_TICKET);
+        }
 
         //Still unsure what EXBB is. Research concluded its
         //`externalblob` binary(36) DEFAULT NULL COMMENT 'sizeof(SceNpId)==36',
         //"SceNpId", Its 36 bytes long, it starts with PSN Username and suffixed with other data in the end
         //This taken straight from https://github.com/hallofmeat/Skateboard3Server/blob/master/src/Skateboard3Server.Blaze/Handlers/Authentication/LoginHandler.cs
-        var externalBlob = new List<byte>();
-        externalBlob.AddRange(Encoding.ASCII.GetBytes(ticket.OnlineId.PadRight(20, '\0')));
-        externalBlob.AddRange(Encoding.ASCII.GetBytes(ticket.Domain));
-        externalBlob.AddRange(Encoding.ASCII.GetBytes(ticket.Region));
-        externalBlob.AddRange(Encoding.ASCII.GetBytes("ps3"));
-        externalBlob.Add(0x0);
-        externalBlob.Add(0x1);
-        externalBlob.AddRange(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
+        // var externalBlob = new List<byte>();
+        // externalBlob.AddRange(Encoding.ASCII.GetBytes(ticket.OnlineId.PadRight(20, '\0')));
+        // externalBlob.AddRange(Encoding.ASCII.GetBytes(ticket.Domain));
+        // externalBlob.AddRange(Encoding.ASCII.GetBytes(ticket.Region));
+        // externalBlob.AddRange(Encoding.ASCII.GetBytes("ps3"));
+        // externalBlob.Add(0x0);
+        // externalBlob.Add(0x1);
+        // externalBlob.AddRange(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
 
         var userIdentification = new UserIdentification
         {
-            mAccountId = (long)ticket.UserId,
+            mAccountId = (long)ticket.SubjectId,
             mAccountLocale = 1701729619,
-            mExternalBlob = externalBlob.ToArray(),
-            mExternalId = ticket.UserId,
-            mBlazeId = (uint)ticket.UserId,
-            mName = ticket.OnlineId,
-            mPersonaId = ticket.OnlineId
+            // mExternalBlob = externalBlob.ToArray(),
+            mExternalId = ticket.SubjectId,
+            mBlazeId = (uint)ticket.SubjectId,
+            mName = ticket.SubjectHandle,
+            mPersonaId = ticket.SubjectHandle
         };
 
         var extendedData = new UserSessionExtendedData
@@ -58,18 +61,18 @@ public class AuthenticationComponent : AuthenticationComponentBase.Server
 
         var sessionInfo = new SessionInfo
         {
-            mBlazeUserId = (uint)ticket.UserId,
-            mSessionKey = ticket.UserId.ToString(),
+            mBlazeUserId = (uint)ticket.SubjectId,
+            mSessionKey = ticket.SubjectId.ToString(),
             mEmail = "",
             mPersonaDetails = new PersonaDetails
             {
-                mDisplayName = ticket.OnlineId,
+                mDisplayName = ticket.SubjectHandle,
                 mLastAuthenticated = 0,
-                mPersonaId = (long)ticket.UserId,
-                mExtId = ticket.UserId,
+                mPersonaId = (long)ticket.SubjectId,
+                mExtId = ticket.SubjectId,
                 mExtType = ExternalRefType.PS3
             },
-            mUserId = (long)ticket.UserId
+            mUserId = (long)ticket.SubjectId
         };
 
         new ServerPlayer(context.BlazeConnection, userIdentification, extendedData, sessionInfo);
